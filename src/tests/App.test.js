d2da023696d1647ec376def105a8f18d7fd88608
Gabriel Galera <gabgalera@hotmail.com>
2022-11-22 18:10:10 -0300

    Testa o filtro de texto

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
 import App from '../App';
 import { mockData } from './mockData'
+import userEvent from '@testing-library/user-event';
 
 test('I am your test', async () => {
   jest.spyOn(global, 'fetch');
@@ -14,5 +15,8 @@ test('I am your test', async () => {
   expect(linkElement).toBeInTheDocument();
 
   await waitForElementToBeRemoved(linkElement)
-  const columns = await screen.findByText(/population/) 
+  const textFilter = await screen.findByTestId(/name-filter/)
+  
+  userEvent.type(textFilter, 'Tat')
+  expect(screen.queryByText(/tatooine/i)).toBeInTheDocument();
 });
