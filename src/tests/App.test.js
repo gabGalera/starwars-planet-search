f5ae57ee317b16ce47fd0ccc41cd103c335f3f81
Gabriel Galera <gabgalera@hotmail.com>
2022-11-22 18:17:37 -0300

    Testa os filtros numéricos

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -19,4 +19,18 @@ test('I am your test', async () => {
   
   userEvent.type(textFilter, 'Tat')
   expect(screen.queryByText(/tatooine/i)).toBeInTheDocument();
+
+  userEvent.type(textFilter, '')
+
+  const columnFilter = await screen.findByTestId(/column-filter/)
+  const compareFilter = await screen.findByTestId(/comparison-filter/)
+  const numberFilter = await screen.findByTestId(/value-filter/)
+  const buttonFilter = await screen.findByTestId(/button-filter/)
+
+  userEvent.selectOptions(columnFilter, 'population')
+  userEvent.selectOptions(compareFilter, 'menor que')
+  userEvent.type(numberFilter, '4500000000')
+  userEvent.click(buttonFilter)
+  screen.debug()
+
 });
