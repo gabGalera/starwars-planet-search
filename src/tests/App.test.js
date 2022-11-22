ef2cc5bbcf8c736167baadcd63954431b98b13d7
Gabriel Galera <gabgalera@hotmail.com>
2022-11-22 18:03:50 -0300

    Faz o mock dos dados

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,18 @@
-// import React from 'react';
-// import { render, screen } from '@testing-library/react';
-// import App from '../App';
+import React from 'react';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import App from '../App';
+import { mockData } from './mockData'
 
-// test('I am your test', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/Hello, App!/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('I am your test', async () => {
+  jest.spyOn(global, 'fetch');
+  global.fetch.mockResolvedValue({
+    json: jest.fn().mockResolvedValue(mockData),
+  });
+
+  render(<App />);
+  const linkElement = screen.getByText(/loading/i);
+  expect(linkElement).toBeInTheDocument();
+
+  await waitForElementToBeRemoved(linkElement)
+  const columns = await screen.findByText(/population/) 
+});
