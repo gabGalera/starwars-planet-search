5e22dab785f47a26b65e38357360ef8cb17e7869
Gabriel Galera <gabgalera@hotmail.com>
2022-11-22 15:25:37 -0300

    Renderiza a tabela

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [planets, setPlanets] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://swapi.dev/api/planets')
+      .then((results) => results.json())
+      .then((data) => setPlanets(
+        data.results.map((entry) => {
+          const { residents, ...others } = entry;
+          return others;
+        }),
+      ))
+      .catch((error) => console.error(error))
+      .then(() => setLoading(false));
+  }, []);
+
+  if (loading) return <h1>Loading...</h1>;
+
   return (
-    <span>Hello, App!</span>
+    <table>
+      <thead>
+        <tr>
+          {Object.keys(planets[0]).map((title) => <th key={ title }>{title}</th>)}
+        </tr>
+      </thead>
+      <tbody>
+        {planets
+          .map((planet) => (
+            <tr key={ planet.name }>
+              {
+                Object.values(planet).map((entry) => <th key={ entry.name }>{entry}</th>)
+              }
+            </tr>
+          ))}
+      </tbody>
+    </table>
   );
 }
 
